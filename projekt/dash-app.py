4934ae14702628ab5d1bf1f7c0f7519d62f61121
notTheLey <cm.ley.cm@gmail.com>
2025-02-05 20:35:27 +0100

    Fix dash-app.py app.runserver()

--- projekt/dash-app.py
+++ projekt/dash-app.py
@@ -113,4 +113,4 @@ def update_graphs(alpha):
     return fig_triangle, fig_sincos, fig_tancot, values_text
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=True, port=8050)
